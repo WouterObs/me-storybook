b8d02f95a7e8e1857c9a48b6ee29e38712c81463
WouterOBS <WouterObs@gmail.com>
2021-06-17 20:20:02 +0200

    Argh

---
+++ storybook-static/precache-manifest.9d7ac366802c54f8a3ef4d17a6d060b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfb336fb83f134fb1875bb9c60967f00",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.db6046a447cde236a43e.bundle.js"
+  },
+  {
+    "url": "runtime~main.db6046a447cde236a43e.bundle.js"
+  },
+  {
+    "revision": "8f6b899855d950c33fa1",
+    "url": "static/css/main.0c6a9cca.chunk.css"
+  },
+  {
+    "url": "vendors~main.db6046a447cde236a43e.bundle.js"
+  },
+  {
+    "url": "vendors~main.db6046a447cde236a43e.bundle.js.LICENSE.txt"
+  }
+]);
