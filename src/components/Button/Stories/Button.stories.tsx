902da3038ae88f1bb8d6047b644be27f36d6b681
WouterOBS <WouterObs@gmail.com>
2021-06-17 20:12:53 +0200

    Latest build / Snapshots

--- src/components/Button/Stories/Button.stories.tsx
+++ src/components/Button/Stories/Button.stories.tsx
@@ -1,27 +1,49 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-import { withKnobs, select, boolean } from "@storybook/addon-knobs";
-import { Button, ButtonProps } from "../../Button/Button";
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { withKnobs, select, boolean } from '@storybook/addon-knobs';
+import { Button, ButtonProps } from '../../Button/Button';
 
 const props: ButtonProps = {
-	primary: true,
-	label: "Button",
-	size: "medium",
+  primary: true,
+  label: 'Button',
+  size: 'medium',
 };
-storiesOf("StyledButton", module)
-	.addDecorator(withKnobs)
-	.add("StyledButton", () => {
-		return (
-			<Button
-				label={props.label}
-				primary={boolean("primary", (props.primary = true))}
-				size={select(
-					"Size",
-					{ Small: "small", Medium: "medium", Large: "large" },
-					props.size
-				)}
-				onClick={action("clicked")}
-			/>
-		);
-	});
+const props2: ButtonProps = {
+  primary: false,
+  backgroundColor: 'white',
+  label: 'Button',
+  size: 'small',
+};
+
+storiesOf('StyledButton', module)
+  .addDecorator(withKnobs)
+  .add('PrimaryButton', () => {
+    return (
+      <Button
+        label={props.label}
+        primary={boolean('primary', props.primary ? true : false)}
+        size={select(
+          'Size',
+          { Small: 'small', Medium: 'medium', Large: 'large' },
+          props.size,
+        )}
+        onClick={action('clicked')}
+      />
+    );
+  })
+  .add('SecondaryButton', () => {
+    return (
+      <Button
+        label={props2.label}
+        primary={boolean('primary', props.primary ? false : true)}
+        backgroundColor={props.primary ? undefined : props2.backgroundColor}
+        size={select(
+          'Size',
+          { Small: 'small', Medium: 'medium', Large: 'large' },
+          props2.size,
+        )}
+        onClick={action('clicked')}
+      />
+    );
+  });
