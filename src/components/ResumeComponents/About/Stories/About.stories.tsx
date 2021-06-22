4b79f870b677d917d16d28f49b8a6902b92702e2
WouterOBS <WouterObs@gmail.com>
2021-06-22 10:21:15 +0200

    Tests
    Test Storyshots updated
    Removed unused action imports

--- src/components/ResumeComponents/About/Stories/About.stories.tsx
+++ src/components/ResumeComponents/About/Stories/About.stories.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { withKnobs, select, boolean } from '@storybook/addon-knobs';
+import { withKnobs } from '@storybook/addon-knobs';
 import About from '../About';
 import { Main } from '../..';
 
